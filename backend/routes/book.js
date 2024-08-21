84b397d7a38c97c23f0d3a4f99e978aff2826c5c
Harsh Roy <harshxroy@gmail.com>
2024-08-22 03:39:07 +0530

    added more apis 2

--- backend/routes/book.js
+++ backend/routes/book.js
@@ -49,4 +49,50 @@ router.put("/update-book", authenticateToken, async (req, res) => {
   }
 });
 
+// delete book - admin
+router.delete("/delete-book", authenticateToken, async (req, res) => {
+  try {
+    const { bookid } = req.headers;
+    await Book.findByIdAndDelete(bookid);
+
+    res.status(200).json({ message: "Book deleted successfully" });
+  } catch (error) {
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+// get all books
+router.get("/get-all-books", async (req, res) => {
+  try {
+    const books = await Book.find().sort({ createdAt: -1 });
+
+    return res.json({ status: "Success", data: books });
+  } catch (error) {
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+// get recently added books , limit 4
+router.get("/get-recent-books", async (req, res) => {
+  try {
+    const books = await Book.find().sort({ createdAt: -1 }).limit(4);
+
+    return res.json({ status: "Success", data: books });
+  } catch (error) {
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+// get book by id
+router.get("/get-book-by-id/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const books = await Book.findById(id);
+    return res.json({ status: "Success", data: books });
+  } catch (error) {
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+
 module.exports = router;
