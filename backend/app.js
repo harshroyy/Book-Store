c6d45be7edd1243b430e927ac4ca679f2a6db762
Harsh Roy <harshxroy@gmail.com>
2024-08-24 13:11:36 +0530

    backend complete

--- backend/app.js
+++ backend/app.js
@@ -2,17 +2,19 @@ const express = require("express");
 const app = express();
 require('dotenv').config();
 require('./conn/conn')
-const user = require("./routes/user")
+const User = require("./routes/user")
 const Books = require("./routes/book")
 const Favourite = require("./routes/favourite")
 const Cart = require("./routes/cart")
+const Order = require("./routes/order")
 
 app.use(express.json());
 // routes
-app.use("/api/v1", user);
+app.use("/api/v1", User);
 app.use("/api/v1", Books);
 app.use("/api/v1", Favourite);
 app.use("/api/v1", Cart);
+app.use("/api/v1", Order);
 
 // listening to port 
 app.listen(process.env.PORT, () => {
---
+++ backend/routes/order.js
@@ -0,0 +1,94 @@
+const router = require("express").Router();
+const { authenticateToken } = require("./userAuth");
+const Books = require("./models/book");
+const Order = require("./models/order");
+const User = require("./routes/user");
+
+// place order
+router.post("/place order", authenticateToken, async (req, res) => {
+  try {
+    const { id } = req.headers;
+    const { order } = req.body;
+
+    for (const orderData of order) {
+      const newOrder = new Order({ user: id, book: orderData._id });
+      const orderDataFromDb = await newOrder.save();
+
+      // saving order in user models
+      await User.findByIdAndUpdate(id, {
+        $push: { order: orderDataFromDb._id },
+      });
+
+      // clearing cart
+      await User.findByIdAndUpdate(id, {
+        $pull: { cart: orderData._id },
+      });
+    }
+    return res.json({
+      status: "Success",
+      message: "Order Placed Succesfully",
+    });
+  } catch (error) {
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+// Get order history
+router.get("/get-order-history", authenticateToken, async (req, res) => {
+  try {
+    const { id } = req.headers;
+
+    // Find all orders for the user, populate the book details
+    const userData = await User.findById(id).populate({
+      path: "orders",
+      populate: { path: "book" },
+    });
+
+    const ordersData = userData.orders.reverse();
+    return res.json({
+      status: "Success",
+      data: ordersData,
+    });
+  } catch (error) {
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+// Get all orders (Admin only)
+router.get("/get-all-orders", authenticateToken, async (req, res) => {
+  try {
+    const userData = await Order.find()
+      .populate({
+        path: "book",
+      })
+
+      .populate({
+        path: "user",
+      })
+
+      .sort({ createdAt: -1 });
+
+    return res.json({
+      status: "Success",
+      data: userData,
+    });
+  } catch (error) {
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+// update order --admin
+router.put("/update-status/:id", authenticateToken, async (req, res) => {
+   try {
+     const {id} = req.params;
+     await Order.findByIdAndUpdate(id, {status: req.body.status})
+     return res.json({
+      status: "Success",
+      message: "Status Updated Successfully",
+    });
+    } catch (error) {
+    return res.status(500).json({ message: "Internal Server Error" });
+   }
+})
+
+module.exports = router;
